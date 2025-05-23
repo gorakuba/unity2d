505d80e79afdef8b206e47af127443952dfdc3be
Damm <damianharanek@wp.pl>
2025-05-23 16:14:23 +0200

    5/6 RedSkull_Threat działa

--- marvel-united/Assets/_Project/Scripts/RedSkullThreat/ThreatAbilityFactory.cs
+++ marvel-united/Assets/_Project/Scripts/RedSkullThreat/ThreatAbilityFactory.cs
@@ -11,6 +11,7 @@ static class ThreatAbilityFactory
         ["Redskull_threat_02"] = typeof(RedskullThreat02),
         ["Redskull_threat_03"] = typeof(RedskullThreat03),
         ["Redskull_threat_04"] = typeof(RedskullThreat04),
+        ["Redskull_threat_05"] = typeof(RedskullThreat05),
     };
 
     public static IThreatAbility Attach(string key, ThreatCardInstance inst)
@@ -24,6 +25,8 @@ static class ThreatAbilityFactory
                 rt4.Init(inst, GameManager.Instance.threatChoicePanel);
             else if (comp is RedskullThreat03 rt3)
                 rt3.Init(inst, null);
+            else if (comp is RedskullThreat05 rt5)
+                rt5.Init(inst);
             return comp;
         }
         return null;
